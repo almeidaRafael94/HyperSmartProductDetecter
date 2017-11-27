5a4e907aa3891ccdfbe4292eb3161a3c8c161b96
rafaelalmeida <ra_fa_94el@hotmail.com>
2017-11-27 09:55:37 +0000

    first commit

---
+++ CMakeFiles/detectionObject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/detectionObject.dir/main.cpp.o"
+  "detectionObject.pdb"
+  "detectionObject"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/detectionObject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
